126d464ed677d0bda97c7da8a7a9480064e7b337
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-03-31 12:18:40 -0500

    created html page using node fs module

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require(`fs`);
+const profileDataArgs = process.argv.slice(2);
 const [name, github] = profileDataArgs;
-const generatePage = (userName, githubName) => {
+const generatePage = (name, github) => {
     return `
     <!DOCTYPE html> 
     <html lang="en"> 
@@ -16,7 +17,10 @@ const generatePage = (userName, githubName) => {
       <h2><a href="https://github.com/${github}">Github</a></h2>
     </body>
     </html>
+  
     `;
 };
-console.log(name, github);
-console.log(generatePage(name, github));
+fs.writeFile(`./index.html`, generatePage(name,github), err => {
+    if (err) throw new Error(err);
+    console.log(`Portfolio complete! Check out index.html to see the output!`);
+});
